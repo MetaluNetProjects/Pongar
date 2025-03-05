8b1081f543238694434c3d59261124162fba1d28
Antoine Rousseau <_antoine_@metalu.net>
2025-03-05 18:03:02 +0100

    add new dmx projectors for testing

--- fw/hw/proj.cpp
+++ fw/hw/proj.cpp
@@ -80,6 +80,52 @@ void Movobeam100::color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
     dmxBuf[dmx_address - 1 + 12] = w;
 }
 
-Movobeam100 proj;
+//---------------------------------------------------------
 
+void Movobeam100Zoom::color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+    dmxBuf[dmx_address - 1 + 10] = r;
+    dmxBuf[dmx_address - 1 + 11] = g;
+    dmxBuf[dmx_address - 1 + 12] = b;
+    dmxBuf[dmx_address - 1 + 13] = w;
+    zoom(255);
+}
+
+void Movobeam100Zoom::zoom(uint8_t z) {
+    dmxBuf[dmx_address - 1 + 15] = z;
+}
+
+//-----------------------------------------------------------
+
+void ClubSpot::move(float pan, float tilt) {
+    pan = CLIP(pan, 0.0, 360.0);
+    tilt = CLIP(tilt, -90.0, 90.0);
+    unsigned int real_pan = pan * (config.proj_pan_amp / 360.0);
+    unsigned int real_tilt = tilt * (27330 / 90.0) + 32768U;
+    dmxBuf[dmx_address - 1 + 1] = real_pan / 256U;
+    dmxBuf[dmx_address - 1 + 2] = real_pan % 256U;
+    dmxBuf[dmx_address - 1 + 3] = real_tilt / 256U;
+    dmxBuf[dmx_address - 1 + 4] = real_tilt % 256U;
+
+    fraise_put_init();
+    fraise_put_uint8(101);
+    fraise_put_int16(pan * 32);
+    fraise_put_int16(tilt * 32);
+    fraise_put_send();
+}
+
+void ClubSpot::dimmer(uint8_t l) {
+    dmxBuf[dmx_address - 1 + 6] = l;
+}
+
+void ClubSpot::color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+    /*dmxBuf[dmx_address - 1 + 9] = r;
+    dmxBuf[dmx_address - 1 + 10] = g;
+    dmxBuf[dmx_address - 1 + 11] = b;
+    dmxBuf[dmx_address - 1 + 12] = w;*/
+}
+
+//-----------------------------------------------------------
+
+ClubSpot proj_impl;
+DMXProj &proj = proj_impl;
 
--- fw/hw/proj.h
+++ fw/hw/proj.h
@@ -25,5 +25,22 @@ public:
     virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 };
 
-extern Movobeam100 proj;
+class Movobeam100Zoom: public Movobeam100 {
+public:
+    //virtual void move(float pan, float tilt);
+    //virtual void dimmer(uint8_t l);
+    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+    void zoom(uint8_t z);
+};
+
+class ClubSpot: public DMXProj {
+public:
+    virtual void move(float pan, float tilt);
+    virtual void dimmer(uint8_t l);
+    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+};
+
+/*typedef ClubSpot PROJ_TYPE;
+extern PROJ_TYPE proj_impl;*/
+extern DMXProj &proj;
 
