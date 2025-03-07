d8d8cbce8fbf3c98b5ed5251c845483a60c1eccb
Antoine Rousseau <_antoine_@metalu.net>
2025-03-07 15:35:26 +0100

    minor cleanup

--- fw/words_def.h
+++ fw/words_def.h
@@ -37,23 +37,11 @@ WORDS_MACRO(une,        31)    // une
 WORDS_MACRO(virgule,    32)
 WORDS_MACRO(centieme,   33)
 WORDS_MACRO(millieme,   34)
-/*
-WORDS_MACRO(et,         21)    // et
-WORDS_MACRO(une,        22)    // une
-WORDS_MACRO(_30,        23)
-WORDS_MACRO(_40,        24)
-WORDS_MACRO(_50,        25)
-WORDS_MACRO(_60,        26)
-WORDS_MACRO(_70,        27)
-WORDS_MACRO(_80,        28)
-WORDS_MACRO(_90,        29)
-WORDS_MACRO(_100,       30)
-WORDS_MACRO(_1000,      31)
-*/
 
 #define WORDS_MULTIPLE_NITEMS 5
 #define WORDS_MULTIPLE_OFFSET 40
 #define WORDS_MULTIPLE(n) (WORDS_MULTIPLE_OFFSET + WORDS_MULTIPLE_NITEMS * n)
+
 WORDS_MACRO(partie                  , WORDS_MULTIPLE(0))
 WORDS_MACRO(joueur                  , WORDS_MULTIPLE(1))
 WORDS_MACRO(niveau                  , WORDS_MULTIPLE(2))
@@ -91,48 +79,3 @@ WORDS_MACRO(vies                    , WORDS_MULTIPLE(32))
 WORDS_MACRO(derniere_vie            , WORDS_MULTIPLE(33)) // attention dernière vie !
 
 #undef WORDS_MACRO // need to redefine WORDS_MACRO each time
-
-/* TODO:
-
-// 43 "phrases" max, 5 choix ( 43 * 5 + 40 = 255)
-
-partie
-joueur
-minute
-seconde
-place
-
-alpague                 // Allez, venez jouer au Pong !
-alpague2
-alpague3
-hiscore_jour            // Nous vous rappelons que le meilleur temps aujourd'hui est de:
-hiscore_general         // Nous vous rappelons que le meilleur temps au classement général est de:
-
-bonjour                 // Bienvenue, placez-vous autour de la piste. Combien de joueurs êtes-vous ?
-attente_joueurs_stables // S'il vous plait, arretez de vous déplacer pour que je puisse savoir combien vous êtes.
-trop_nombreux           // Attention vous êtes trop nombreux, 4 joueurs max.
-début_partie            // Très bien la partie va pouvoir commencer. Vous êtes : ("un" "joueur")
-gagne_niveau_1          // Bien bien, vous avez reporté cette première manche ! Préparez-vous pour la prochaine, ça va être un peu plus difficile. Vous avez trois vies.
-gagne_niveau_2          // Bravo, vous avez reporté la deuxième manche ! Attention, la troisième est vraiment coton.
-perdu_niveau            // Dommage, vous avez perdu. Mais vous pouvez réessayer : il vous reste deux vies.
-perdu_niveau_derniere   // Encore perdu ! Attention c'est votre dernière chance ! Vous n'avez plus qu'une vie !
-gagne_partie            // Bravo, vous avez remporté la dernière manche ! Votre temps est de : (Pour remporter ces trois manches, vous avez mis :).
-classement_jour         // Votre place dans le classement du jour est :
-champion_jour           // 1: bravo meilleur score de la journée ! 2: incroyable, deuxième place du jour ! etc.
-classement_general      // Vous êtes rentré dans le classement général ; vous avez conquis la place numéro :
-champion                // 1: bravo champion du monde ! 2: incroyable, deuxième place ! etc. jusque 5
-perdu_niveau1           // Vous avez perdu dès le premier niveau, laissez la place à des meilleurs que vous.
-perdu_partie            // Zut, c'était votre dernière chance, et vous avez perdu la partie ! N'hésitez pas à réessayer une prochaine fois.
-
-*/
-
-/*
-WORDS_MACRO(partie                  , 101)
-WORDS_MACRO(joueur                  , 102)
-WORDS_MACRO(perdu                   , 103)
-WORDS_MACRO(gagne                   , 104)
-WORDS_MACRO(niveau                  , 105)
-WORDS_MACRO(vies                    , 106)
-WORDS_MACRO(derniere_vie            , 107) // attention dernière vie !
-*/
-
