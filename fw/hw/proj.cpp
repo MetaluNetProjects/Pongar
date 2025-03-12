b4ed19218ec0ed51fd777f346e78f6e74a4bef7c
Antoine Rousseau <_antoine_@metalu.net>
2025-03-12 17:15:05 +0100

    proj: new projectors, implement inertia compensation, implement color wheel command

--- fw/hw/proj.cpp
+++ fw/hw/proj.cpp
@@ -8,47 +8,59 @@ extern unsigned char dmxBuf[];
 
 #define CLIP(x, min, max) MAX(MIN((x), (max)), (min))
 
-/*void proj_goto(float pan, float tilt) {
+void DMXProjCompens::move(float pan, float tilt) {
     pan = CLIP(pan, 0.0, 360.0);
     tilt = CLIP(tilt, -90.0, 90.0);
-    unsigned int real_pan = pan * (config.proj_pan_amp / 360.0);
-    unsigned int real_tilt = tilt * (27330 / 90.0) + 32768U;
-    dmxBuf[PROJ_DMX_OFFSET + 0] = real_pan / 256U;
-    dmxBuf[PROJ_DMX_OFFSET + 1] = real_pan % 256U;
-    dmxBuf[PROJ_DMX_OFFSET + 2] = real_tilt / 256U;
-    dmxBuf[PROJ_DMX_OFFSET + 3] = real_tilt % 256U;
+
+    target_pan = pan * (config.proj_pan_amp / 360.0);
+    target_tilt = tilt * (27330 / 90.0) + 32768U;
+
     fraise_put_init();
     fraise_put_uint8(101);
     fraise_put_int16(pan * 32);
     fraise_put_int16(tilt * 32);
     fraise_put_send();
+    if(!compens) {
+        dmx_pan = target_pan;
+        dmx_tilt = target_tilt;
+        sendmove();
+    }
 }
 
-void proj_set_light(uint8_t l) {
-    dmxBuf[PROJ_DMX_OFFSET + 7] = l;
-}*/
+void DMXProjCompens::update() {
+    if(!compens) return;
+    estimated_pan = estimated_pan + (target_pan - estimated_pan) * gain_pan;
+    dmx_pan = CLIP(target_pan * 2 - estimated_pan, 0, 65535);
+    estimated_tilt = estimated_tilt + (target_tilt - estimated_tilt) * gain_tilt;
+    dmx_tilt = CLIP(target_tilt * 2 - estimated_tilt, 0, 65535);
+    sendmove();
+}
 
-void MaxiSpot60::move(float pan, float tilt) {
-    pan = CLIP(pan, 0.0, 360.0);
-    tilt = CLIP(tilt, -90.0, 90.0);
-    unsigned int real_pan = pan * (config.proj_pan_amp / 360.0);
-    unsigned int real_tilt = tilt * (27330 / 90.0) + 32768U;
-    dmxBuf[dmx_address - 1 + 1] = real_pan / 256U;
-    dmxBuf[dmx_address - 1 + 2] = real_pan % 256U;
-    dmxBuf[dmx_address - 1 + 3] = real_tilt / 256U;
-    dmxBuf[dmx_address - 1 + 4] = real_tilt % 256U;
-
-    fraise_put_init();
-    fraise_put_uint8(101);
-    fraise_put_int16(pan * 32);
-    fraise_put_int16(tilt * 32);
-    fraise_put_send();
+void MaxiSpot60::sendmove() {
+    dmxBuf[dmx_address - 1 + 1] = dmx_pan / 256U;
+    dmxBuf[dmx_address - 1 + 2] = dmx_pan % 256U;
+    dmxBuf[dmx_address - 1 + 3] = dmx_tilt / 256U;
+    dmxBuf[dmx_address - 1 + 4] = dmx_tilt % 256U;
 }
 
 void MaxiSpot60::dimmer(uint8_t l) {
     dmxBuf[dmx_address - 1 + 8] = l;
 }
+
 void MaxiSpot60::color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {}
+void MaxiSpot60::color(Color col) {
+    uint8_t vcol = 0;
+    switch(col) {
+        case white:     vcol = 0; break;
+        case red:       vcol = 18 * 1 + 4; break;
+        case blue:      vcol = 18 * 3 + 4; break;
+        case orange:    vcol = 126; break;
+        case green:     vcol = 18 * 2 + 4; break;
+        case yellow:    vcol = 18 * 4 + 4; break;
+        //case purple:    vcol = 18 * 5 + 4; break;
+    }
+    dmxBuf[dmx_address - 1 + 6] = vcol;
+}
 
 //---------------------------------------------------------
 
@@ -126,6 +138,57 @@ void ClubSpot::color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
 
 //-----------------------------------------------------------
 
-ClubSpot proj_impl;
+void EVOBeam::sendmove() {
+    dmxBuf[dmx_address - 1 + 1] = dmx_pan / 256U;
+    dmxBuf[dmx_address - 1 + 2] = dmx_pan % 256U;
+    dmxBuf[dmx_address - 1 + 3] = dmx_tilt / 256U;
+    dmxBuf[dmx_address - 1 + 4] = dmx_tilt % 256U;
+}
+
+void EVOBeam::move(float pan, float tilt) {
+    pan = CLIP(pan, 0.0, 360.0);
+    tilt = CLIP(tilt, -90.0, 90.0);
+
+    target_pan = pan * (config.proj_pan_amp / 360.0);
+    target_tilt = tilt * (27330 / 90.0) + 32768U;
+
+    fraise_put_init();
+    fraise_put_uint8(101);
+    fraise_put_int16(pan * 32);
+    fraise_put_int16(tilt * 32);
+    fraise_put_send();
+}
+
+void EVOBeam::dimmer(uint8_t l) {
+    dmxBuf[dmx_address - 1 + 8] = l;
+}
+
+void EVOBeam::color(Color col) {
+    uint8_t vcol = 0;
+    switch(col) {
+        case white: vcol = 0; break;
+        case red: vcol = 10; break;
+        case blue: vcol = 20; break;
+        case orange: vcol = 80; break;
+        case green: vcol = 30; break;
+        case yellow: vcol = 40; break;
+        //case lightpurple: vcol = 60; break;
+        //case lightblue: vcol = 70; break;
+    }
+    dmxBuf[dmx_address - 1 + 5] = vcol;
+    dmxBuf[dmx_address - 1 + 7] = 50; // open shutter
+}
+
+void EVOBeam::update() {
+    estimated_pan = estimated_pan + (target_pan - estimated_pan) * gain_pan;
+    dmx_pan = CLIP(target_pan * 2 - estimated_pan, 0, 65535);
+    estimated_tilt = estimated_tilt + (target_tilt - estimated_tilt) * gain_tilt;
+    dmx_tilt = CLIP(target_tilt * 2 - estimated_tilt, 0, 65535);
+    sendmove();
+}
+
+//-----------------------------------------------------------
+
+MaxiSpot60 proj_impl;
 DMXProj &proj = proj_impl;
 
--- fw/hw/proj.h
+++ fw/hw/proj.h
@@ -6,26 +6,65 @@ class DMXProj {
 protected:
     int dmx_address = 1;
 public:
+    enum Color { white, red, blue, orange, green, yellow };
     virtual void move(float pan, float tilt) = 0;
     virtual void dimmer(uint8_t l) = 0;
-    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w) = 0;
+    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w = 0) {};
+    virtual void color(Color col) {
+        switch(col) {
+            case white: color(255, 255, 255); break;
+            case red: color(255, 0, 0); break;
+            case blue: color(0, 0, 255); break;
+            case orange: color(255, 128, 0); break;
+            case green: color(0, 255, 0); break;
+            case yellow: color(255, 255, 0); break;
+        }
+    };
+    virtual void update() {}
 };
 
-class MaxiSpot60: public DMXProj {
+class DMXProjCompens: public DMXProj {
+protected:
+    int target_pan, target_tilt;
+    int estimated_pan, estimated_tilt;
+    int dmx_pan, dmx_tilt;
+    float gain_pan = 0.1, gain_tilt = 0.1;
+    bool compens = true;
+    virtual void sendmove() = 0;
+public:
+    virtual void move(float pan, float tilt);
+    virtual void update();
+
+    void set_gains(float pan, float tilt) { 
+        gain_pan = pan;
+        gain_tilt = tilt;
+    }
+    void enable_compens(bool enable) { 
+        compens = enable;
+    }
+};
+
+class MaxiSpot60: public DMXProjCompens { // un peu trop lent, flou
+private:
+    virtual void sendmove();
+public:
+    MaxiSpot60() {
+        gain_pan = 0.3;
+        gain_tilt = 0.1;
+    }
+    virtual void dimmer(uint8_t l);
+    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+    virtual void color(Color col);
+};
+
+class Movobeam100: public DMXProj { // moteurs ok, mais net impossible
 public:
     virtual void move(float pan, float tilt);
     virtual void dimmer(uint8_t l);
     virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 };
 
-class Movobeam100: public DMXProj {
-public:
-    virtual void move(float pan, float tilt);
-    virtual void dimmer(uint8_t l);
-    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
-};
-
-class Movobeam100Zoom: public Movobeam100 {
+class Movobeam100Zoom: public Movobeam100 { // n'apporte rien sur le non-zoom
 public:
     //virtual void move(float pan, float tilt);
     //virtual void dimmer(uint8_t l);
@@ -33,14 +72,30 @@ public:
     void zoom(uint8_t z);
 };
 
-class ClubSpot: public DMXProj {
+class ClubSpot: public DMXProj { // figure nette, mais trop lent.
 public:
     virtual void move(float pan, float tilt);
     virtual void dimmer(uint8_t l);
     virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 };
 
-/*typedef ClubSpot PROJ_TYPE;
-extern PROJ_TYPE proj_impl;*/
+class EVOBeam: public DMXProj { // figure nette, mais trop lent.
+    int target_pan, target_tilt;
+    int estimated_pan, estimated_tilt;
+    int dmx_pan, dmx_tilt;
+    float gain_pan = 0.06, gain_tilt = 0.03;
+    void sendmove();
+public:
+    virtual void move(float pan, float tilt);
+    virtual void dimmer(uint8_t l);
+    virtual void color(uint8_t r, uint8_t g, uint8_t b, uint8_t w){}
+    virtual void color(Color col);
+    virtual void update();
+    void set_gains(float pan, float tilt) { 
+        gain_pan = pan;
+        gain_tilt = tilt;
+    }
+};
+
 extern DMXProj &proj;
 
