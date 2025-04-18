7a0eacc3104455667012cc3c78cccdc5ef78864c
Antoine Rousseau <_antoine_@metalu.net>
2025-04-18 17:26:46 +0200

    update config about spot leds, and add a projector intensity setting
    Signed-off-by: Antoine Rousseau <_antoine_@metalu.net>

--- fw/config.h
+++ fw/config.h
@@ -9,7 +9,7 @@ public:
     void eeprom_declare();
     void receivebytes(const char* data, uint8_t len);
     // data:
-    uint16_t total_leds = 212;
+    uint16_t ring_leds = 212;
     int16_t leds_angle_offset = 0; //112;
 
     int16_t distance_max = 1700;
@@ -20,6 +20,11 @@ public:
     uint16_t proj_pan_amp = 44850;
 
     int16_t game_distance_min = 700;
+
+    uint8_t volume = 120;
+    uint8_t spot_leds = 4;
+
+    uint8_t proj_lum = 150;
 };
 
 extern PongarConfig config;
--- fw/hw/config.cpp
+++ fw/hw/config.cpp
@@ -12,11 +12,11 @@ void PongarConfig::eeprom_declare() {
 }
 
 void PongarConfig::pixel_show(int from, int to) {
-    //from = from % total_leds;
-    //to = to % total_leds;
+    //from = from % ring_leds;
+    //to = to % ring_leds;
     for(int i = 0; i < NUM_PIXELS - 1; i++) {
-        if(i >= from && i <= to) set_pixel(i, 255, 255, 255);
-        else set_pixel(i, 0, 0, 0);
+        if(i >= from && i <= to) set_ring_pixel(i, 255, 255, 255);
+        else set_ring_pixel(i, 0, 0, 0);
     }
 }
 
@@ -24,13 +24,13 @@ void PongarConfig::receivebytes(const char* data, uint8_t len) {
     char command = fraise_get_uint8();
     switch(command) {
     case 1: {
-        total_leds = fraise_get_uint16();
-        pixel_show(0, total_leds - 1);
+        ring_leds = fraise_get_uint16();
+        pixel_show(0, ring_leds - 1);
     }
     break;
     case 2: {
         leds_angle_offset = fraise_get_int16();
-        int led = (((leds_angle_offset + 360) % 360) * total_leds) / 360;
+        int led = (((leds_angle_offset + 360) % 360) * ring_leds) / 360;
         pixel_show(led, led);
     }
     break;
@@ -52,16 +52,26 @@ void PongarConfig::receivebytes(const char* data, uint8_t len) {
     case 8:
         game_distance_min = fraise_get_uint16();
         break;
+    case 9:
+        volume = fraise_get_uint8();
+        break;
+    case 10:
+        spot_leds = fraise_get_uint8();
+        break;
+    case 11:
+        proj_lum = fraise_get_uint8();
+        break;
     case 100:
-        printf("cfg %d %d %d %d %d %d %d\n",
-               total_leds, leds_angle_offset,
+        printf("cfg %d %d %d %d %d %d %d %d %d %d\n",
+               ring_leds, leds_angle_offset,
                distance_max, distance_min,
-               lidar_angle_offset, proj_tilt_amp, proj_pan_amp);
+               lidar_angle_offset, proj_tilt_amp, proj_pan_amp,
+               volume, spot_leds, proj_lum);
         break;
     case 101:
         fraise_put_init();
         fraise_put_uint8(200);
-        fraise_put_int16(total_leds);
+        fraise_put_int16(ring_leds);
         fraise_put_int16(leds_angle_offset);
         fraise_put_int16(distance_max);
         fraise_put_int16(distance_min);
@@ -69,6 +79,9 @@ void PongarConfig::receivebytes(const char* data, uint8_t len) {
         fraise_put_int16(proj_tilt_amp);
         fraise_put_uint16(proj_pan_amp);
         fraise_put_uint16(game_distance_min);
+        fraise_put_uint8(volume);
+        fraise_put_uint8(spot_leds);
+        fraise_put_uint8(proj_lum);
         fraise_put_send();
         break;
     }
